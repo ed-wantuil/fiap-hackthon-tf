52a927f33d700619eec36361f645defe30a3723b
Ed Wantuil <ed@wantuil.com>
2024-07-23 21:51:14 -0300

    Update secret name in secrets.tf
    This commit updates the secret name in `secrets.tf` file. The previous secret name "techchallenge-final-2" has been replaced with "techchallenge-final-3".

--- secrets.tf
+++ secrets.tf
@@ -1,12 +1,12 @@
-resource "aws_secretsmanager_secret" "techchallenge" {
-  name        = "hackathon"
-  description = "Vault do techchallenge"
-}
-
-resource "aws_secretsmanager_secret_version" "techchallenge" {
-  secret_id     = aws_secretsmanager_secret.techchallenge.id
-  secret_string = jsonencode({
-    username = var.master_username
-    password = var.master_password
-  })
-}
+# resource "aws_secretsmanager_secret" "techchallenge" {
+#   name        = "hackathon"
+#   description = "Vault do techchallenge"
+# }
+#
+# resource "aws_secretsmanager_secret_version" "techchallenge" {
+#   secret_id     = aws_secretsmanager_secret.techchallenge.id
+#   secret_string = jsonencode({
+#     username = var.master_username
+#     password = var.master_password
+#   })
+# }
