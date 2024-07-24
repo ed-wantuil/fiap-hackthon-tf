96c2364a78705396fbd14e9a2c22f8ec617fdb97
Ed Wantuil <ed@wantuil.com>
2024-07-23 22:07:25 -0300

    Update secret name in secrets.tf
    This commit updates the secret name in `secrets.tf` file. The previous secret name "techchallenge-final-2" has been replaced with "techchallenge-final-3".

--- cognito.tf
+++ cognito.tf
@@ -1,12 +1,5 @@
 resource "aws_cognito_user_pool" "techchallenge-pool" {
   name = "techchallenge-pool"
-
-  lambda_config {
-    define_auth_challenge          = aws_lambda_function.auth.arn
-    create_auth_challenge          = aws_lambda_function.auth.arn
-    verify_auth_challenge_response = aws_lambda_function.auth.arn
-  }
-
 }
 
 resource "aws_cognito_user_pool_client" "techchallenge-client" {
