a7c0511751910abaf4bfa5578afc951c171b42b2
Ed Wantuil <ed@wantuil.com>
2024-08-05 20:39:17 -0300

    Uncomment EKS resources and add SQS queues
    Uncommented the definitions for EKS cluster and node group in eks.tf to enable their deployment. Additionally, included the configuration for three new SQS queues in sqs.tf to facilitate message queuing within the application.

---
+++ sqs.tf
@@ -0,0 +1,23 @@
+resource "aws_sqs_queue" "tech_challenge_producao" {
+  name                      = "tech_challenge_producao"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  visibility_timeout_seconds = 30
+}
+
+resource "aws_sqs_queue" "tech_challenge_pagamento" {
+  name                      = "tech_challenge_pagamento"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  visibility_timeout_seconds = 30
+}
+
+resource "aws_sqs_queue" "tech_challenge_pagamento_status" {
+  name                      = "tech_challenge_pagamento_status"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  visibility_timeout_seconds = 30
+}
